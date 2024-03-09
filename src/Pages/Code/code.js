bc92c1aaca834564f3cf2bfb33e01d36f3736353
Martiwj <Martin.wich.jorg@gmail.com>
2024-03-09 14:19:41 +0100

    routing between pages added

---
+++ src/Pages/Code/code.js
@@ -0,0 +1,13 @@
+import { Link } from 'react-router-dom';
+
+
+function Code() {
+  return (
+    <div className="Code">
+        
+        <h1>Hello World</h1>
+    </div>
+  );
+}
+
+export default Code;
